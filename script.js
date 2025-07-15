5bf978a92e53cb7d0ab83b81575ff8084f2f1f8f
viniciusmachadovianna <viniciusmachadovianna.dev@gmail.com>
2025-07-14 22:49:21 -0300

    feat: mock chat + send messages via log

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('loaded');
+document.getElementById('btnSend').addEventListener('click',function (){ sendMessage(this)
+})
+function sendMessage(){
+  const msg = document.getElementById('msgInput')
+  console.log(msg.value)
+}
