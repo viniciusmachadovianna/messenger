dcd4b33effa7a611db4d64501f847735f58079fe
viniciusmachadovianna <viniciusmachadovianna.dev@gmail.com>
2025-07-15 16:28:41 -0300

    feat: add attachment menu

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-document.getElementById('btnSend').addEventListener('click',function (){ sendMessage()
-})
+const btnSendMessage = document.getElementById('btnSendMessage'),
+  btnAddAttachment = document.getElementById('btnAddAttachment')
+
+btnAddAttachment.addEventListener('click',()=>toggleAttachmentMenu())
+btnSendMessage.addEventListener('click',()=>sendMessage())
+
+function toggleAttachmentMenu(){
+  console.log('menu');
+}
 function sendMessage(){
   const chat = document.getElementById('chat'),
     input = document.getElementById('msgInput'),
@@ -7,7 +14,7 @@ function sendMessage(){
     span = document.createElement('span'),
     hour = document.createElement('sub'),
     d= new Date()
-  hour.textContent = `${d.getHours()}:${d.getMinutes()}`
+  hour.textContent = `${d.getHours().toString().padStart(2, '0')}:${d.getMinutes().toString().padStart(2, '0')}`
   span.textContent=input.value
   message.appendChild(span)
   message.appendChild(hour)
