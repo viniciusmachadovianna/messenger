97b5db07dbb3a5e34491669338a849f353f08541
viniciusmachadovianna <viniciusmachadovianna.dev@gmail.com>
2025-07-15 17:12:15 -0300

    solid: isolate input content validation

--- script.js
+++ script.js
@@ -8,23 +8,25 @@ function toggleAttachmentMenu(){
   console.log('menu');
 }
 function sendMessage(){
-  const input = document.getElementById('msgInput'),
-    text = document.getElementById('msgInput').value
-  if(!text.trim()) return
+  const input = document.getElementById('msgInput')
+  if(isEmpty(input)) return
+  
   const chat = document.getElementById('chat'),
     message = document.createElement('div'),
     span = document.createElement('span'),
     hour = document.createElement('sub'),
     d= new Date()
   hour.textContent = `${d.getHours().toString().padStart(2, '0')}:${d.getMinutes().toString().padStart(2, '0')}`
-  span.textContent=text
+  span.textContent=input.value
   message.appendChild(span)
   message.appendChild(hour)
   message.className='historyMessage msgSender'
   chat.appendChild(message)
   clearInput(input)
 }
-
+function isEmpty(input){
+  return !input.value.trim();
+}
 function clearInput(input){
   input.value = ''
   input.style.height = 'auto'
